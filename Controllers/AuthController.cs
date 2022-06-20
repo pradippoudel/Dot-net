b1af45606b95d61140fb93ba2141f6c4d2e71fe1
aishwarya <aish.stha07@gmail.com>
2022-06-20 07:05:24 +0545

    Feature: Dot net project frontend completed

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pradip_project.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult Signup()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        public ViewResult Aboutus()
+        {
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+        public ViewResult Listofitem()
+        {
+            return View();
+        }
+
+        public ViewResult Trendingoftheweek()
+        {
+            return View();
+        }
+
+
+    }
+}
+
---
+++ Controllers/DescriptionController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pradip_project.Controllers
+{
+    public class DescriptionController1 : Controller
+    {
+        public ViewResult Samsung()
+        {
+            return View();
+        }
+    }
+}
